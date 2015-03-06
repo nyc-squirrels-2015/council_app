1079c9f27341a4a0efcee42f2bb7f00086d97d10
Anna Taberski <aftaberski@gmail.com>
2015-03-06 12:52:09 -0500

    add basic CRD operations and routes

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,39 @@
+class QuestionsController < ApplicationController
+
+  def index
+    @questions = Question.all
+  end
+
+  def show
+    @question = Question.find params[:id]
+  end
+
+  def new
+    @question = Question.new
+  end
+
+  def create
+    @question = Question.new question_params
+    if @question.save
+      redirect_to question_path(@question)
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+    Question.find(params[:id]).destroy
+    redirect_to questions_path
+  end
+
+private
+  def question_params
+    params.require(:question).permit(:user_id, :council_id, :content)
+  end
+end
