e24a53c13f37541a98c46d4940b377111d2d1dd5
sagararathi <bravo.sagar@gmail.com>
2015-03-06 14:30:55 -0500

    Add routes to controller

--- app/controllers/councils_controller.rb
+++ app/controllers/councils_controller.rb
@@ -4,6 +4,46 @@ class CouncilsController < ApplicationController
     @councils = Council.all
   end
 
-  def
+  def new
+    @council = Council.new
+  end
+
+  def create
+    @council = Council.where(council_params).first_or_create
+    if @council
+      redirect_to @council
+    else
+      render :new
+    end
+  end
+
+  def edit
+    find_council(params[:id])
+  end
+
+  def update
+    find_council(params[:id])
+    if @council.update_attributes(council_params)
+      redirect_to @council
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    council = Council.find_by(id: params[:id])
+
+    council.destroy
+    redirect_to councils_path
+  end
+
+  private
+  def council_params
+   params.require(:council).permit(:council_name).merge(user_id: 1)
+  end
+
+  def find_council id
+    @council = Council.find(id)
+  end
 
 end
