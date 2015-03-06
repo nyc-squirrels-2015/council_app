8a45238daa0a40e7910daf20f57ff81b155b5201
Melissa Wimberly <melissa.f.wimberly@gmail.com>
2015-03-06 13:13:34 -0500

    Add answers resource

--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,7 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
+  resources :answers
   root :to => 'welcome#index'
 
 
