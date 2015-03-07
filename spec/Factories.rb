3422a00533737530667955744dc1eed9119576c5
sagararathi <bravo.sagar@gmail.com>
2015-03-07 12:28:25 -0500

    Add factorgirl for user and question

--- spec/Factories.rb
+++ spec/Factories.rb
@@ -1,7 +1,22 @@
 # Add FactoryGirl for council controller
 FactoryGirl.define do
+  factory :user do |f|
+    f.firstname {Faker::Name.first_name}
+    f.lastname {Faker::Name.last_name}
+    f.email {"test@example.com"}
+    f.password {"admin"}
+  end
+
   factory :council do |f|
     f.council_name { Faker::Commerce.department }
-    f.user_id { 1 }
+    f.association :user
   end
+
+  factory :question do |f|
+    f.content {Faker::Lorem.sentence}
+    f.image {Faker::Avatar.image}
+    f.association :council
+    f.association :user
+  end
+
 end
