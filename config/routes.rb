2ee4609b5205b1b5d777ec7ad624fd69a6870a31
Melissa Wimberly <melissa.f.wimberly@gmail.com>
2015-03-07 13:42:53 -0500

    Bootstrap in progress

--- config/routes.rb
+++ config/routes.rb
@@ -16,6 +16,7 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
   
   root :to => 'welcome#index'
+  # root :to => 'index'
 
 
   # Example of regular route:
