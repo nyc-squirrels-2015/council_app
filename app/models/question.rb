dd38ff987556394d4ae2e0d99bbe4b6059dbbf7d
sagararathi <bravo.sagar@gmail.com>
2015-03-08 13:44:42 -0400

    Fixed code logic for up_percentage

--- app/models/question.rb
+++ app/models/question.rb
@@ -16,7 +16,11 @@ class Question < ActiveRecord::Base
 
   def up_percentage
     all_votes = self.answers.count
-    up_votes = self.answers.where(like: true).count
-    ((up_votes.to_f / all_votes.to_f) * 100).round
+    unless all_votes.zero?
+      up_votes = self.answers.where(like: true).count
+      ((up_votes.to_f / all_votes.to_f) * 100).round
+    else
+      up_votes = 0
+    end
   end
 end
