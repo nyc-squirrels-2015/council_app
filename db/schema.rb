e906fd43d8631b7b6858886c24a60ba5e38ebf70
sagararathi <bravo.sagar@gmail.com>
2015-03-08 17:00:14 -0400

    Update schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150305212150) do
+ActiveRecord::Schema.define(version: 20150308183129) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -35,10 +35,11 @@ ActiveRecord::Schema.define(version: 20150305212150) do
   end
 
   create_table "friendships", force: :cascade do |t|
-    t.integer  "user_id",    null: false
-    t.integer  "friend_id",  null: false
+    t.integer  "user_id",                    null: false
+    t.integer  "friend_id",                  null: false
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.boolean  "status",     default: false
   end
 
   create_table "questions", force: :cascade do |t|
