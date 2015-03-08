9a769f617ad0e07fe73b0e11c8369f62831f1d20
sagararathi <sagararathi@users.noreply.github.com>
2015-03-08 13:50:37 -0400

    Revert "Profile page"

--- app/models/question.rb
+++ app/models/question.rb
@@ -16,11 +16,7 @@ class Question < ActiveRecord::Base
 
   def up_percentage
     all_votes = self.answers.count
-    unless all_votes.zero?
-      up_votes = self.answers.where(like: true).count
-      ((up_votes.to_f / all_votes.to_f) * 100).round
-    else
-      up_votes = 0
-    end
+    up_votes = self.answers.where(like: true).count
+    ((up_votes.to_f / all_votes.to_f) * 100).round
   end
 end
