002a5b97b39023bf5421488b0c7e392af12940bd
Anna Taberski <aftaberski@gmail.com>
2015-03-08 17:01:34 -0400

    Add ability to swipe to vote

--- app/assets/javascripts/votes.js
+++ app/assets/javascripts/votes.js
@@ -3,13 +3,15 @@ $( document ).ready(function() {
   $(".vote-swipe").swipe({
     swipe:function(event, direction) {
       if ('right' == direction) {
-        $('#answer_like').checked = true;
+        console.log("right");
+        $('#answer_like').prop('checked', true);
+      }
+      else if ('left' == direction) {
+        console.log("left");
+        $('#answer_like').prop('checked', false);
       }
       $('#new_answer').submit();
-
-
     }
-
   });
 
   // $(".vote-swipe").swipe({
