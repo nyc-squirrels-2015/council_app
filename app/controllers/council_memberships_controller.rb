33415f9bf4e77a78c6a756ad0840bb7f657fdf49
Anna Taberski <aftaberski@gmail.com>
2015-03-07 23:28:29 -0500

    Add Council Memberships Controller

--- app/controllers/council_memberships_controller.rb
+++ app/controllers/council_memberships_controller.rb
@@ -1,23 +1,25 @@
 class CouncilMembershipsController < ApplicationController
 
 	def index
-		@council_memberships = CouncilMemberships.all
+		@council_memberships = CouncilMembership.where(user_id: current_user.id)
 	end
 
-	def new	
-		@council_memberships = CouncilMemberships.new
+	def new
+		@council_membership = CouncilMembership.new
 	end
 
 	def create
-		@council_memberships = CouncilMemberships.where(council_params).first_or_create
-    if @council
-      redirect_to @council
+		@council_membership = CouncilMembership.where(council_membership_params).first_or_create
+    if @council_membership.save
+      redirect_to councils_path
     else
       render :new
-    end	
+    end
 	end
 
 private
-
+  def council_membership_params
+    params.require(:council_membership).permit(:user_id, :council_id)
+  end
 
 end
