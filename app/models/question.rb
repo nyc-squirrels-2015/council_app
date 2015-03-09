eb06c57a96675680db5a67a6f7bb927bb905a4c6
Anna Taberski <aftaberski@gmail.com>
2015-03-09 09:35:00 -0400

    Update answers for questions

--- app/models/question.rb
+++ app/models/question.rb
@@ -8,18 +8,18 @@ class Question < ActiveRecord::Base
   validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
   # accepts_nested_attributes_for :answers
   validates :council_id, :user_id, :content, presence: true
-  
+
 
   def s3_credentials
     {bucket: "council-app", access_key_id: ENV["AWS_KEY"],secret_access_key: ENV["AWS_SECRET"] }
   end
 
   def up_percentage
-    unless self.answers.count.zero?
-      up_votes = self.answers.where(like: true).count
-      ((up_votes.to_f / self.answers.where(like: true).count.to_f) * 100).round
+    if self.answers.count.zero?
+      return 0
     else
-      up_votes = 0
+      up_votes = self.answers.where(like: true).count
+      ((up_votes.to_f / self.answers.count.to_f) * 100).round
     end
   end
 end
