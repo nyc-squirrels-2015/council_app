7847c112d46b9ba6bec84bbf2fe2b3de0a89e172
Jay <jhd@fastmail.net>
2015-03-10 11:10:11 -0400

    Add custom validations

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -1,15 +1,34 @@
+class CantAnswerOwnQuestion < ActiveModel::Validator
+  def validate(record)
+    if record.question.user == record.user
+      record.errors[:own_question] << "You can't answer your own question."
+    end
+  end
+end
+
+class CantAnswerTwice < ActiveModel::Validator
+  def validate(record)
+    if record.question.already_answered(record.user.id)
+      record.errors[:duplicate] << "You can't answer more than once."
+    end
+  end
+end
+
+
 class Answer < ActiveRecord::Base
+  include ActiveModel::Validations
+  
   belongs_to :question
   belongs_to :user
 
   validates :question_id, :user_id, presence: true
   validates :like, inclusion: { in: [true, false] }
+  validates_with CantAnswerOwnQuestion
+  validates_with CantAnswerTwice
 
- #  def answers
- #    self.hashtags.pluck(:name)
- #  end
+ # question's user != user
 
- #  def self.ordered_json
- #  	order("created_at DESC").limit(50).to_json(methods: :answers)
-	# end
+ # question doesn't have another answer with this user
+ # !question.already_answered(user.id)
+
 end
