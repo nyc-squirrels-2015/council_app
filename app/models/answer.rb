350754f84f61b36ba2702e2b04c29a7f662a3df1
Jay <jhd@fastmail.net>
2015-03-10 13:59:07 -0400

    Rewrite custom validations, add tests

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -1,34 +1,25 @@
-class CantAnswerOwnQuestion < ActiveModel::Validator
-  def validate(record)
-    if record.question.user == record.user
-      record.errors[:own_question] << "You can't answer your own question."
-    end
-  end
-end
-
-class CantAnswerTwice < ActiveModel::Validator
-  def validate(record)
-    if record.question.already_answered(record.user.id)
-      record.errors[:duplicate] << "You can't answer more than once."
-    end
-  end
-end
-
-
 class Answer < ActiveRecord::Base
   include ActiveModel::Validations
   
   belongs_to :question
   belongs_to :user
 
-  validates :question_id, :user_id, presence: true
+  validates :question, :user, presence: true
   validates :like, inclusion: { in: [true, false] }
-  validates_with CantAnswerOwnQuestion
-  validates_with CantAnswerTwice
 
- # question's user != user
+  validate :not_own_question
+  validate :cant_answer_twice
+  
+  def cant_answer_twice 
+    if question && question.already_answered(user.id)
+      errors[:duplicate] <<  "You can't answer more than once."
+    end
+  end
 
- # question doesn't have another answer with this user
- # !question.already_answered(user.id)
-
+  def not_own_question
+    if question && question.user && question.user == user
+      errors[:own_question] << "You can't answer your own question."
+    end
+  end
+ 
 end
